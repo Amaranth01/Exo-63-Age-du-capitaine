2df0e547b43d2270c74b0af277da14b2456ffe4d
Amaranth01 <vanessa.nectoux@gmail.com>
2021-11-15 19:45:17 +0100

    Exo 63 ok

---
+++ app.js
@@ -0,0 +1,14 @@
+for(let i=0; i < 19; i +=2)
+{
+    document.getElementById('capitaine').innerHTML+="Le capitaine a "+i+" ans, le capitaine est jeune <br><br>";
+}
+
+for(let i=19; i < 50; i +=5)
+{
+    document.getElementById('capitaine').innerHTML+="Le capitaine a "+i+" ans, n'est pas très vieux <br><br>";
+}
+
+for(let i=50; i < 51; i +=5)
+{
+    document.getElementById('capitaine').innerHTML+="Le capitaine a "+i+" ans, le capitaine est vieux <br><br>";
+}
